943a909a04dae352419aaea2eea976ff598d8bb6
Ben Clarke <rod.parfait@gmail.com>
2012-10-24 15:34:07 -0400

    Added autoresponder table

--- sql/update_db.sql
+++ sql/update_db.sql
@@ -6,6 +6,18 @@ create table domain_admins (
 
 alter table virtual_users add column super_admin boolean default false;
 
+CREATE TABLE `autoresponder` (
+	`email` varchar(255) NOT NULL default '',
+	`descname` varchar(255) default NULL,
+	`from` date NOT NULL default '0000-00-00',
+	`to` date NOT NULL default '0000-00-00',
+	`message` text NOT NULL,
+	`enabled` tinyint(4) NOT NULL default '0',
+	`subject` varchar(255) NOT NULL default '',
+	PRIMARY KEY (`email`),
+	FULLTEXT KEY `message` (`message`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 create table autoresponder_recipients (
 	user_email varchar(255) not null references autoresponder(email),
 	recipient_email varchar(255) not null,
