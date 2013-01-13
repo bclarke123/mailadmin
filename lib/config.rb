a84f217ce1ae9a3a13d457ca9eb0cb2c66950185
Alexander Stintzing <a.stintzing@metaways.de>
2013-01-13 17:14:28 +0100

    make it more flexible
    	- allow to set main table names
    	- allow to force host, port, scheme in building links to allow using
    	  the app behind a transparent proxy
    	- added configuration hint for using this app behind a proxy to config.ru
    	- removed datepickr.js call, it's not available

--- lib/config.rb
+++ lib/config.rb
@@ -6,6 +6,15 @@ module MailConfig
 	DB_PASS = ""
 	DB_DB = "mailserver"
 	
+	TABLE_ADMINS  = "domain_admins"
+	TABLE_USERS   = "virtual_users"
+	TABLE_DOMAINS = "virtual_domains"
+	TABLE_ALIASES = "virtual_aliases"
+  
+	HTTP_SCHEME = 'http'
+	HTTP_HOST   = 'localhost'
+	HTTP_PORT   = 4567
+	
 # If you're going to use the built-in autoresponder, this should return a
 # user's maildir mailbox when %u is replaced with their username, and %d with
 # the domain
