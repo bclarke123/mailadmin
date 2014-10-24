0e33209d7a208c403790f2c99f78d94a3120f949
Kuraiko Yoshikawa <kuraiko@timesfactory.com>
2014-10-24 12:51:10 +0200

    add action to password sheme methods

--- lib/password_shemes.rb
+++ lib/password_shemes.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
+require 'digest'
+require 'base64'
+require 'securerandom'
+
 
 class PassShemes 
   ############################################
@@ -6,7 +10,9 @@ class PassShemes
   #
   # See also http://wiki2.dovecot.org/Authentication/PasswordSchemes
   #
-  
+  @@hash_lengh = { 'smd5' => 16, 'sshd' => 20, 'ssha256' => 32, 'ssha512' => 64 }
+  @@salt_length = 8
+
   def initialize(password, hash)
     @password = password
     @hash     = hash
@@ -17,28 +23,27 @@ class PassShemes
   
   # MD5 sum of the password stored in hex. 
   def plain_md5
-  
-    return "{PLAIN-MD5}"
+    return "{PLAIN-MD5}" + Digest::MD5.hexdigest(@password)
   end
   
   # MD5 sum of the password stored in base64. 
   def ldap_md5
-  
+    return "{LDAP-MD5}" + Digest::MD5.base64digest(@password)
   end
   
   # SHA1 sum of the password stored in base64. 
   def sha
-  
+    return "{SHA}" + Digest::SHA1.base64digest(@password)
   end
   
   # SHA256 sum of the password stored in base64. (v1.1 and later). 
   def sha256
-  
+    return "{SHA256}" + Digest::SHA256.base64digest(@password)
   end
   
   # SHA512 sum of the password stored in base64. (v2.0 and later). 
   def sha512
-  
+    return "{SHA512}" + Digest::SHA512.base64digest(@password)
   end
   
   # Salted password shemes 
@@ -46,22 +51,31 @@ class PassShemes
   
   # Salted MD5 sum of the password stored in base64. 
   def smd5
-  
+    salt = make_salt()
+    return "{SMD5}" + Base64.urlsafe_encode64(Digest::MD5.digest(@password + salt ) + salt)
   end
   
   # Salted SHA1 sum of the password stored in base64. 
   def ssha
-  
+    salt = make_salt()
+    return "{SSHA}" + Base64.urlsafe_encode64(Digest::SHA1.digest(@password + salt ) + salt)
   end
   
   # Salted SHA256 sum of the password stored in base64. (v1.2 and later). 
   def ssha256
-  
+    salt = make_salt()
+    return "{SSHA256}" + Base64.urlsafe_encode64(Digest::SHA256.digest(@password + salt ) + salt)
   end
   
   # Salted SHA512 sum of the password stored in base64. (v2.0 and later). 
   def ssha512
-  
+    salt = make_salt()
+    "{SSHA512}" + Base64.urlsafe_encode64(Digest::SHA512.digest(@password + salt ) + salt)
+  end
+
+
+  def make_salt() 
+    return SecureRandom.random_bytes(@@salt_length)
   end
 
 end
